dcd8126ef45be0824f84f100a52019a3eae7d6bb
Ivo Gulin <69385144+IvoGulin@users.noreply.github.com>
2020-11-20 12:37:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ var images = [
 	'images/2.jpg', 
 	'images/3.jpg'
 	];
-	var num = 0;
-	function next() {
+	
+var num = 0;
+function next() 
+{
 	var slider = document.getElementById('slider');
 	num++;
-	if(num >= images.length) {
-	num = 0;
+	if(num >= images.length) 
+	{
+		num = 0;
 	}
 	slider.src = images[num];
-	}
-	function prev() {
+}
+
+function prev() 
+{
 	var slider = document.getElementById('slider');
 	num--;
-	if(num < 0) {
-	num = images.length-1;
+	if(num < 0) 
+	{
+		num = images.length-1;
 	}
 	slider.src = images[num];
-	}
+}
